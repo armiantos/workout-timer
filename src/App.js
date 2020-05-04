2e17f4ea658de1e6f91d717b5de1e1478f98cb7d
armiantos <armiantos@gmail.com>
2020-05-04 11:01:47 -0400

    Removed React header and added simple styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Timer from './Timer';
@@ -7,23 +6,7 @@ import Timer from './Timer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className="fullscreen">
         <Timer/>
-      </div>
     </div>
   );
 }
--- src/Timer.js
+++ src/Timer.js
@@ -7,7 +7,10 @@ import {
   ThemeProvider,
   createMuiTheme,
   IconButton,
+  Divider,
 } from "@material-ui/core";
+
+import {makeStyles} from '@material-ui/core/styles';
 
 import AddAlarmIcon from "@material-ui/icons/AddAlarm";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
@@ -15,14 +18,25 @@ import PauseIcon from "@material-ui/icons/Pause";
 
 import { blue, yellow } from "@material-ui/core/colors";
 
-import "./Timer.css";
-
 const theme = createMuiTheme({
   palette: {
     primary: blue,
     secondary: yellow,
   },
 });
+
+const useStyles = makeStyles({
+  root: {
+    'max-width': '400px' 
+  },
+  m: {
+    margin: '20px',
+    'background-color': 'black'
+  },
+  s: {
+    margin: '6px'
+  }
+})
 
 function TimerControls(props) {
   return (
@@ -35,7 +49,7 @@ function TimerControls(props) {
 function TimerInput(props) {
   return (
     <Container>
-      <TextField label="Time" variant="outlined" />
+      <TextField label="Time" />
       <IconButton onClick={props.callback}>
         <AddAlarmIcon />
       </IconButton>
@@ -61,24 +75,33 @@ function Timer() {
     if (active)
       interval = setInterval(() => {
         setTimers((timers) =>
-          timers.map((val, index) => (!index ? val - 1 : val)).filter(val => val >= 0)
+          timers
+            .map((val, index) => (!index ? val - 1 : val))
+            .filter((val) => val >= 0)
         );
-      }, 1000); 
-    
+      }, 1000);
+
     if (timers.length < 1) setActive(false);
-    
+
     return () => {
       clearInterval(interval);
     };
   }, [active, timers.length]);
 
+  const classes = useStyles();
+
   return (
     <ThemeProvider theme={theme}>
-      <Card className="Timer-App">
-        <TimerControls callback={toggleTimer} active={active} />
-        <TimerInput callback={addTimer} />
-        {timers.map((duration) => (
-          <Box>{duration}</Box>
+      <Card className={classes.root}>
+        <TimerControls callback={toggleTimer} active={active} className={classes.m}/>
+        <Divider />
+        <TimerInput callback={addTimer} className={classes.m}/>
+        <Divider />
+        {timers.map((duration, index) => (
+          <Box key={index} className={classes.s}>
+            <Box>{duration}s</Box>
+            <Divider />
+          </Box>
         ))}
       </Card>
     </ThemeProvider>
